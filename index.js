3aaab8066397f9aeff026c9346c060b7d042ac2f
真田悟司 <sanadasatorushi@sanadasatorushinoMacBook-Air.local>
2021-10-10 18:47:36 +0900

    Add

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const question = 'ゲーム市場、最も売れたゲーム機は次の内どれ？';
-const answes = [
+const answers = [
   'スーパーファミコン',
   'プレーステション2',
   'ニンテンドースイッチ',
   'ニンテンドーDS'
 ];
 const correct = 'ニンテンドーDS';
+
+document.getElementById('js-question').textContent = question;
+
+document.getElementsByTagName('button')[0].textContent = answers[0];
+document.getElementsByTagName('button')[1].textContent = answers[1];
+document.getElementsByTagName('button')[2].textContent = answers[2];
+document.getElementsByTagName('button')[3].textContent = answers[3];
+
