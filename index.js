1ec449cec0d688d7af10c001ff3b6a44834512e3
真田悟司 <sanadasatorushi@sanadasatorushinoMacBook-Air.local>
2021-10-10 18:55:17 +0900

    Add

--- index.js
+++ index.js
@@ -7,10 +7,21 @@ const answers = [
 ];
 const correct = 'ニンテンドーDS';
 
+//定数の文字列をHTMLに反映させる
 document.getElementById('js-question').textContent = question;
 
-document.getElementsByTagName('button')[0].textContent = answers[0];
-document.getElementsByTagName('button')[1].textContent = answers[1];
-document.getElementsByTagName('button')[2].textContent = answers[2];
-document.getElementsByTagName('button')[3].textContent = answers[3];
+const $button = document.getElementsByTagName('button');
 
+$button[0].textContent = answers[0];
+$button[1].textContent = answers[1];
+$button[2].textContent = answers[2];
+$button[3].textContent = answers[3];
+
+//ボタンをクリックしたら生後判定
+$button[0].addEventListener('click', () => {
+  if( correct === document.getElementsByTagName('button')[0].textContent ){
+    window.alert('正解！');
+  } else {
+    window.alert('不正解！');
+  }
+});
