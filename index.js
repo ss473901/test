13d918bbd35e581a9394088c31b528f51879c9ea
真田悟司 <sanadasatorushi@sanadasatorushinoMacBook-Air.local>
2021-10-10 19:26:41 +0900

    Add

--- index.js
+++ index.js
@@ -7,42 +7,33 @@ const answers = [
 ];
 const correct = 'ニンテンドーDS';
 
-//定数の文字列をHTMLに反映させる
-document.getElementById('js-question').textContent = question;
-
 const $button = document.getElementsByTagName('button');
+const buttonLength = $button.length;
 
-$button[0].textContent = answers[0];
-$button[1].textContent = answers[1];
-$button[2].textContent = answers[2];
-$button[3].textContent = answers[3];
+//クイズの問題文、選択肢を定義
+const setupQuiz = () => {
+  document.getElementById('js-question').textContent = question;
+  let buttonIndex = 0;
+  while(buttonIndex < buttonLength){
+    $button[buttonIndex].textContent = answers[buttonIndex];
+    buttonIndex++;
+  }
+}
+setupQuiz();
+
+const clickHandler = (e) => {
+  if( correct === e.target.textContent ){
+    window.alert('正解！');
+  } else {
+    window.alert('不正解！');
+  }
+};
 
 //ボタンをクリックしたら生後判定
-$button[0].addEventListener('click', () => {
-  if( correct === $button[0].textContent ){
-    window.alert('正解！');
-  } else {
-    window.alert('不正解！');
-  }
-});
-$button[1].addEventListener('click', () => {
-  if( correct === $button[1].textContent ){
-    window.alert('正解！');
-  } else {
-    window.alert('不正解！');
-  }
-});
-$button[2].addEventListener('click', () => {
-  if( correct === $button[2].textContent ){
-    window.alert('正解！');
-  } else {
-    window.alert('不正解！');
-  }
-});
-$button[3].addEventListener('click', () => {
-  if( correct === $button[3].textContent ){
-    window.alert('正解！');
-  } else {
-    window.alert('不正解！');
-  }
-}); 
+let handlerIndex = 0;
+while (handlerIndex < buttonLength) {
+  $button[handlerIndex].addEventListener('click', (e) => {
+    clickHandler(e);
+  });
+  handlerIndex++;
+}
